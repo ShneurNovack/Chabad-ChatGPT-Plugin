529d686a753b102f0eeeefeb2706af929a924ccd
ShneurNovack <81717928+ShneurNovack@users.noreply.github.com>
2023-06-16 00:33:50 -0400

    Update centers.js

--- functions/centers.js
+++ functions/centers.js
@@ -1,24 +1,27 @@
-// pages/api/centers.js
-export default async (request, response) => {
-  // Prepare the API URL
-  let api_url = 'https://www.chabad.org/api/v2/chabadorg/centers/'
+// worker.js
+export async function onRequest(context) {
+    const api_url = 'https://www.chabad.org/api/v2/chabadorg/centers/';
+    const request = context.request;
+    
+    if (request.method === 'GET') {
+        try {
+            const apiRes = await fetch(api_url, {
+                method: request.method,
+                headers: request.headers
+            });
 
-  // Check if the request method is GET
-  if (request.method === 'GET') {
-    let apiResponse = await fetch(api_url, {
-      method: request.method,
-      headers: request.headers
-    })
+            // Pass the response back to the browser
+            return new Response(apiRes.body, {
+                status: apiRes.status,
+                statusText: apiRes.statusText,
+                headers: apiRes.headers
+            });
 
-    // Check if the API request is successful
-    if (apiResponse.ok) {
-      const data = await apiResponse.json();
-      response.status(200).json(data);
+        } catch (error) {
+            return new Response('Error calling API', { status: 500 });
+        }
     } else {
-      response.status(apiResponse.status).send('Error calling API');
+        // If the method is not GET
+        return new Response(`Method ${request.method} Not Allowed`, { status: 405 });
     }
-  } else {
-    // For non-GET requests, simply respond with method not allowed
-    response.status(405).send('Method not allowed');
-  }
 }
